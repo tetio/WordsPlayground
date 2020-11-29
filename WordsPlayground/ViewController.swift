239a6442cb16d03e57c4f651692c9464ae445afc
Sergi Maymi <tetio@icloud.com>
2020-11-29 22:17:50 +0100

    Initial Commit

---
+++ WordsPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordsPlayground
+//
+//  Created by Sergi Maymi on 29/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
